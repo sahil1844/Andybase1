be031db2e2b09f6fcab92a6c091454e3a1377eaa
sahil1844 <sahilrupareliya3535@gmail.com>
2024-07-24 12:58:59 +0530

    set scrollicon

--- app/src/main/java/com/example/andybase/BarsActivity.java
+++ app/src/main/java/com/example/andybase/BarsActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.ScrollView;
 import android.widget.VideoView;
@@ -27,6 +30,75 @@ public class BarsActivity extends AppCompatActivity {
             @Override
             public void run() {
                 scrollView.scrollTo(0, 0);
+            }
+        });
+        ImageView scrollIcon = findViewById(R.id.scrollIcon);
+        scrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                int scrollMax = scrollView.getChildAt(0).getHeight() - scrollView.getHeight();
+
+                // Listen for scroll changes to update the scrollIcon position
+                scrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+                    @Override
+                    public void onScrollChanged() {
+                        // Get the current scroll position
+                        int scrollY = scrollView.getScrollY();
+
+                        // Calculate the maximum translation Y for the icon (within bounds)
+                        float maxTranslationY = scrollView.getHeight() - scrollIcon.getHeight();
+
+                        // Calculate the translation proportionally
+                        float iconTranslationY = (scrollY / (float) scrollMax) * maxTranslationY;
+
+                        // Update the icon position
+                        scrollIcon.setTranslationY(iconTranslationY);
+                    }
+                });
+
+                // Allow the icon to be draggable as well
+                scrollIcon.setOnTouchListener(new View.OnTouchListener() {
+                    float initialY;
+                    float initialIconY;
+                    float scrollStartY;
+
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN:
+                                // Record the initial touch position and current scroll position
+                                initialY = event.getRawY();
+                                initialIconY = scrollIcon.getTranslationY();
+                                scrollStartY = scrollView.getScrollY();
+                                return true;
+
+                            case MotionEvent.ACTION_MOVE:
+                                // Calculate the difference in Y positions
+                                float deltaY = event.getRawY() - initialY;
+
+                                // Calculate the new scroll position
+                                float newScrollPosition = scrollStartY + deltaY * (scrollMax / (float)(scrollView.getHeight() - scrollIcon.getHeight()));
+
+                                // Ensure the new scroll position is within bounds
+                                if (newScrollPosition < 0) {
+                                    newScrollPosition = 0;
+                                } else if (newScrollPosition > scrollMax) {
+                                    newScrollPosition = scrollMax;
+                                }
+
+                                // Programmatically scroll the ScrollView
+                                scrollView.scrollTo(0, (int) newScrollPosition);
+
+                                // Update the icon position proportionally
+                                float maxTranslationY = scrollView.getHeight() - scrollIcon.getHeight();
+                                float iconTranslationY = (newScrollPosition / scrollMax) * maxTranslationY;
+                                scrollIcon.setTranslationY(iconTranslationY);
+
+                                return true;
+                        }
+                        return false;
+                    }
+                });
             }
         });
         v1 = findViewById(R.id.actionbar);
--- app/src/main/java/com/example/andybase/GallaryBottomnavi.java
+++ app/src/main/java/com/example/andybase/GallaryBottomnavi.java
@@ -2,9 +2,13 @@ package com.example.andybase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.ScrollView;
 import android.widget.Toast;
@@ -26,7 +30,76 @@ public class GallaryBottomnavi extends AppCompatActivity {
                 scrollView.scrollTo(0, 0);
             }
         });
+        ImageView scrollIcon = findViewById(R.id.scrollIcon);
+        scrollView.post(new Runnable() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public void run() {
+                int scrollMax = scrollView.getChildAt(0).getHeight() - scrollView.getHeight();
 
+                // Listen for scroll changes to update the scrollIcon position
+                scrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+                    @Override
+                    public void onScrollChanged() {
+                        // Get the current scroll position
+                        int scrollY = scrollView.getScrollY();
+
+                        // Calculate the maximum translation Y for the icon (within bounds)
+                        float maxTranslationY = scrollView.getHeight() - scrollIcon.getHeight();
+
+                        // Calculate the translation proportionally
+                        float iconTranslationY = (scrollY / (float) scrollMax) * maxTranslationY;
+
+                        // Update the icon position
+                        scrollIcon.setTranslationY(iconTranslationY);
+                    }
+                });
+
+                // Allow the icon to be draggable as well
+                scrollIcon.setOnTouchListener(new View.OnTouchListener() {
+                    float initialY;
+                    float initialIconY;
+                    float scrollStartY;
+
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN:
+                                // Record the initial touch position and current scroll position
+                                initialY = event.getRawY();
+                                initialIconY = scrollIcon.getTranslationY();
+                                scrollStartY = scrollView.getScrollY();
+                                return true;
+
+                            case MotionEvent.ACTION_MOVE:
+                                // Calculate the difference in Y positions
+                                float deltaY = event.getRawY() - initialY;
+
+                                // Calculate the new scroll position
+                                float newScrollPosition = scrollStartY + deltaY * (scrollMax / (float)(scrollView.getHeight() - scrollIcon.getHeight()));
+
+                                // Ensure the new scroll position is within bounds
+                                if (newScrollPosition < 0) {
+                                    newScrollPosition = 0;
+                                } else if (newScrollPosition > scrollMax) {
+                                    newScrollPosition = scrollMax;
+                                }
+
+                                // Programmatically scroll the ScrollView
+                                scrollView.scrollTo(0, (int) newScrollPosition);
+
+                                // Update the icon position proportionally
+                                float maxTranslationY = scrollView.getHeight() - scrollIcon.getHeight();
+                                float iconTranslationY = (newScrollPosition / scrollMax) * maxTranslationY;
+                                scrollIcon.setTranslationY(iconTranslationY);
+
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+            }
+        });
         videoView = findViewById(R.id.vv);
         Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.gallryview);
         videoView.setVideoURI(uri);
